80808f1907c31e168c2ae323f28556975dd5dbbd
ThangLee123 <gemi220909@gmail.com>
2025-05-24 11:05:09 +0700

    Testing Husky & lint-staged

---
+++ src/app/page1.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const page1 = () => {
+  const testFunc = () => {console.log( "test" );};
+  testFunc();
+  return (
+    <div>page1</div>
+  )
+}
+
+export default page1
