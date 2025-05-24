258f0a2d7fb0440db952fbb152a852a15411b026
ThangLee123 <gemi220909@gmail.com>
2025-05-24 09:28:43 +0700

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-console": "warn", // Warn on console.log usage
+    },
+  },
 ];
 
 export default eslintConfig;
